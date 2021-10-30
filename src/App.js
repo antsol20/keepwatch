bdce6c1f37561f500b12973e41e4e76560fe8950
antsol20 <antsol20@hotmail.com>
2021-10-30 23:48:23 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 import HeaderComp from './components/headcomp';
 import Watchlist from './components/watchlist';
 import Addwatch from './components/addwatch';
 
 function App() {
+
+  const [watches, setWatches] = useState([])
+
+  useEffect(() => {
+    const getWatches = async () => {
+
+      const watchesFromServer = await fetchWatches()
+      setWatches(watchesFromServer)
+    }
+
+    getWatches()
+  }, [])
+
+
+  const fetchWatches = async () => {
+    const url = "https://cors-anywhere.herokuapp.com/https://keepwatch.page/api"
+    const res = await fetch(url, { headers: { 'Authorization': 'Basic YnVsbDpidWxsMTIz' } })
+    const data = await res.json()
+
+    return data
+  }
+
   return (
     <div className="App">
-      
+
       <HeaderComp />
-      <Watchlist />
+      <Watchlist watches={watches}/>
       <br></br>
       <Addwatch />
 
--- src/components/watchlist.js
+++ src/components/watchlist.js
@@ -1,32 +1,12 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
 
-const entries =
-    [
-        {
-            "Description": "Savoy",
-            "Url": "https://www.gordonramsayrestaurants.com/savoy-grill/whats-on/",
-            "hash": "109976c1a6af80af0427b699f499287d"
-        },
-        {
-            "Description": "Savoy",
-            "Url": "https://www.gordonramsayrestaurants.com/savoy-grill/menus/",
-            "hash": "f9fcafb2cec13812c4ddd8fea294952f"
-        },
-        {
-            "Description": "Savoy",
-            "Url": "https://www.gordonramsayrestaurants.com/savoy-grill/",
-            "hash": "a417c003e59054bb56193a0d5961157b"
-        }
-    ]
+const Watchlist = ({watches}) => {
 
-
-
-const Watchlist = () => {
     return (
 
         <div class="wrapper">
-            {entries.map((entry) => (<><div className="one">{entry.Description}</div>
+            {watches.map((entry) => (<><div className="one">{entry.Description}</div>
                 <div className="two">{entry.Url}</div><div className="three"><FaTimes style={{color: 'red'}}/></div>
             </>
             ))}
